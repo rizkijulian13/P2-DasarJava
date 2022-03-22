6dea48898fe5e661708cc00755a1750343322f94
rizkijulian13 <13erje13>
2022-03-22 14:55:03 +0700

    menggunakan percabangan if lanjutan

---
+++ src/Percabangan2IF.java
@@ -0,0 +1,29 @@
+import java.util.Scanner;
+
+public class Percabangan2IF {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        System.out.print("Enter a number : ");
+        int intNumber = input.nextInt();
+        if (intNumber > 5){
+            System.out.println("The Number you entered is greater than 5!");
+        }
+        System.out.println("Thanks for the input!");
+
+        System.out.print("\nEnter a number between 10-20 : ");
+        int a = input.nextInt();
+        if (a >= 10 && a <= 20){
+            System.out.println("The Condition has been met");
+        } else {
+            System.out.println("You did it wrong");
+        }
+
+        System.out.print("\nEnter a number between 10-20 or 30-40 : ");
+        int b = input.nextInt();
+        if (((b) >= 10) && (b <= 20) || ((b >= 30) && (b <= 40))){
+            System.out.println("The Condition has been met");
+        } else {
+            System.out.println("You did it wrong");
+        }
+    }
+}
