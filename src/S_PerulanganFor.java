49843a498c98482f1ce6670bc8bada1e9fa24533
rizkijulian13 <13erje13>
2022-03-23 19:41:25 +0700

    menggunakan perulangan for

---
+++ src/S_PerulanganFor.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+
+public class S_PerulanganFor {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        System.out.print("Masukan Jumlah Perulangan : ");
+        int jml = input.nextInt();
+        for (int i = 0; i < jml; i++)
+            System.out.println("Pemogrman Beroriientasi Objek Dengan Java");
+    }
+}
